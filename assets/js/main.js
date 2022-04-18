926d0c05b04590bce1b3ebed3e863a7bc4a38a7f
Masatoshi N <nishiguchi.masa@gmail.com>
2022-04-18 09:03:27 -0400

    Remove unused JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-// https://www.chartjs.org/docs/3.6.1/getting-started/integration.html#bundlers-webpack-rollup-etc
-import ChartJS from 'chart.js/auto'
-import Navbar from './navbar'
 // https://listjs.com/
 import ListJS from 'list.js'
 
-Navbar.init()
-
-window.ChartJS = ChartJS
 window.ListJS = ListJS
