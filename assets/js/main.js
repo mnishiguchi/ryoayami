a9efe94ed72ab8e1e0b02af010ad3c8943135ef8
Masatoshi N <nishiguchi.masa@gmail.com>
2022-04-18 19:17:47 -0400

    Install party.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,14 @@
 import ListJS from 'list.js'
 
 window.ListJS = ListJS
+
+// https://party.js.org
+document.addEventListener('click', (event) => {
+  if (event.target.nodeName !== 'IMG') return
+
+  window.party.confetti(event.target, {
+    count: party.variation.range(30, 60),
+    spread: party.variation.range(40, 80),
+    size: 1.5
+  })
+})
